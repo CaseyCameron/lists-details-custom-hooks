d2097eaf414d93d1bad207f95019480b2c5ef928
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-05 10:14:39 -0700

    App tests passing

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, cleanup, waitFor } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('App component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('Displays a list of characters on the home page', async () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    screen.getByText('Loading...');
+    const ul = await screen.findByRole('list');
+    expect(ul).not.toBeEmptyDOMElement();
+  });
+
+  it('displays a characters details', async () => {
+    render(
+      <MemoryRouter initialEntries={['/5cf5679a915ecad153ab68d1']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    await screen.findByText('Analay');
   });
 });
