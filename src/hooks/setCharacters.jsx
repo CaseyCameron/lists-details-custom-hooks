02fbfc4a98f484d19ce1f377641d0959de695240
Casey Cameron <mcasey.cameron@gmail.com>
2021-08-04 17:02:35 -0700

    custom hooks

--- src/hooks/setCharacters.jsx
+++ src/hooks/setCharacters.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { fetchCharacters, fetchCharacterById } from '../services/CharacterFetch';
 
-export const setCharacters = () => {
+export const useCharacters = () => {
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
 
@@ -11,4 +11,13 @@ export const setCharacters = () => {
       .finally(() => setLoading(false));
   }, [page]);
   return { loading, characters };
+};
+
+export const useCharacter = (id) => {
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    fetchCharacterById(id).then(setCharacter);
+  }, []);
+  return character;
 };
